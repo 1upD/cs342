cbfa724c2ed320319daa41f8b589fcf48b0debfa
Derek Dik <drd26@calvin.edu>
2017-03-03 16:43:25 -0500

    Implemented schema.

---
+++ project/drop.sql
@@ -0,0 +1,14 @@
+-- Drop entity relationship tables
+DROP TABLE ReviewChapter;
+DROP TABLE IntroducedElement;
+DROP TABLE TestedElement;
+
+-- Drop entity tables
+DROP TABLE GameplayElement;
+DROP TABLE GameplayCategory;
+DROP TABLE Review;
+DROP TABLE Chapter;
+DROP TABLE Game;
+DROP TABLE Engine;
+DROP TABLE Player;
+
---
+++ project/schema.sql
@@ -0,0 +1,80 @@
+CREATE TABLE Player(
+	playerID INTEGER PRIMARY KEY,
+	username VARCHAR(32)
+);
+
+CREATE TABLE Engine(
+	engineID INTEGER PRIMARY KEY,
+	engineName VARCHAR(32),
+	dateReleased DATE
+);
+
+CREATE TABLE Game(
+	gameID INTEGER PRIMARY KEY,
+	gameName VARCHAR(32),
+	dateReleased DATE,
+	engineID INTEGER,
+	FOREIGN KEY (engineID) REFERENCES Engine(engineID)
+);
+
+CREATE TABLE Review(
+	reviewID INTEGER PRIMARY KEY,
+	playerID INTEGER,
+	gameID INTEGER,
+	dateSubmitted DATE,
+	rating INTEGER,
+	reviewComment VARCHAR(512),
+	FOREIGN KEY (playerID) REFERENCES Player(playerID),
+	FOREIGN KEY (gameID) REFERENCES Game(gameID)
+);
+
+CREATE TABLE Chapter(
+	chapterName VARCHAR(32),
+	chapterNumber INTEGER,
+	gameID INTEGER,
+	PRIMARY KEY (chapterName, gameID),
+	FOREIGN KEY (gameID) REFERENCES Game(gameID)
+);
+
+CREATE TABLE GameplayCategory(
+	categoryID INTEGER PRIMARY KEY,
+	categoryName VARCHAR(32)
+);
+
+CREATE TABLE GameplayElement(
+	gameplayElementID INTEGER PRIMARY KEY,
+	gameplayElementName VARCHAR(32),
+	categoryID INTEGER,
+	FOREIGN KEY (categoryID) REFERENCES GameplayCategory(categoryID)
+);
+
+CREATE TABLE ReviewChapter(
+	reviewID INTEGER,
+	chapterNumber INTEGER,
+	minutesPlayed INTEGER,
+	datePlayed DATE,
+	rating INTEGER,
+	chapterComment VARCHAR(512),
+	PRIMARY KEY (reviewID, chapterNumber),
+	FOREIGN KEY (reviewID) REFERENCES Review(reviewID) 
+	-- There needs to be a constraint that the chapter number must actually correspond to a chapter of the given game
+);
+
+
+CREATE TABLE IntroducedElement(
+	reviewID INTEGER,
+	gameplayElementID,
+	introducedElementComment VARCHAR(256),
+	PRIMARY KEY (reviewID, gameplayElementID),
+	FOREIGN KEY (reviewID) REFERENCES Review(reviewID),
+	FOREIGN KEY (gameplayElementID) REFERENCES GameplayElement(gameplayElementID)
+);
+
+CREATE TABLE TestedElement(
+	reviewID INTEGER,
+	gameplayElementID,
+	testedElementComment VARCHAR(256),
+	PRIMARY KEY (reviewID, gameplayElementID),
+	FOREIGN KEY (reviewID) REFERENCES Review(reviewID),
+	FOREIGN KEY (gameplayElementID) REFERENCES GameplayElement(gameplayElementID)
+);
