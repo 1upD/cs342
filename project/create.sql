7b5349a14ff6135002757b0ba057fcab05ce54c1
Derek Dik <drd26@calvin.edu>
2017-05-17 20:47:24 -0400

    Documented project and added additional constraints

--- project/create.sql
+++ project/create.sql
@@ -1,8 +1,10 @@
 -- Create users.
 DROP USER lddbUser CASCADE;
-CREATE USER lddbUser IDENTIFIED BY bjarne;
+CREATE USER lddbUser IDENTIFIED BY bjarne; -- This password is a test password and is not for production.
+-- These permissions are required by the scripts that load the database and it's procedures, triggers, and views
 GRANT CREATE SESSION, CREATE TABLE, CREATE PROCEDURE, CREATE TRIGGER, CREATE VIEW, CREATE MATERIALIZED VIEW, UNLIMITED TABLESPACE, CREATE SEQUENCE TO lddbUser;
 
+-- Create the directory for the datapump
 DROP DIRECTORY exp_dir;
 CREATE DIRECTORY exp_dir AS 'C:\Users\drd26\Documents\project';
 GRANT READ, WRITE ON DIRECTORY exp_dir to lddbUser;
--- project/schema.sql
+++ project/schema.sql
@@ -1,14 +1,25 @@
+-- Create the schema for the level design database.
+
+-- Table represents a user that can write reviews of games for the level design database.
+-- Satisfies 4NF (see data.txt)
 CREATE TABLE Player(
 	playerID INTEGER PRIMARY KEY,
 	username VARCHAR(32)
 );
 
+-- Table representing the game engine used for any games or mods in the database.
+-- Since the database was intended to be used for Source engine mods, it is important to specify
+-- which engine was used to create the mod. Currently the database does not suppport related
+-- versions of an engine such as the 2007 Source Engine vs the 2013 Source Engine.
+-- Satisfies 4NF (see data.txt)
 CREATE TABLE Engine(
 	engineID INTEGER PRIMARY KEY,
 	engineName VARCHAR(64),
 	dateReleased DATE
 );
 
+-- Table representing a game, expansion pack, or game mod. Each game can have a URL and an engine ID
+-- Satisfies 4NF (see data.txt)
 CREATE TABLE Game(
 	gameID INTEGER PRIMARY KEY,
 	gameName VARCHAR(32),
@@ -18,6 +29,12 @@ CREATE TABLE Game(
 	FOREIGN KEY (engineID) REFERENCES Engine(engineID)
 );
 
+-- Review table models a review of a game by a user. It could have been defined as a weak entity 
+-- identified by playerID and gameID, however giving Review it's own reviewID simplifies other tables 
+-- and additionally means a user can create multiple reviews of the same game if the game changes or 
+-- over time.
+-- Constraints ensure that the rating of a game is always between 0 and 10.
+-- Satisfies 4NF (see data.txt)
 CREATE TABLE Review(
 	reviewID INTEGER PRIMARY KEY,
 	playerID INTEGER,
@@ -27,7 +44,9 @@ CREATE TABLE Review(
 	reviewComment VARCHAR(512),
 	FOREIGN KEY (playerID) REFERENCES Player(playerID),
 	FOREIGN KEY (gameID) REFERENCES Game(gameID),
-	UNIQUE (playerID, gameID, dateSubmitted)
+	UNIQUE (playerID, gameID, dateSubmitted),
+	CHECK (rating >= 0),
+	CHECK (rating <= 10) -- Ratings will go from 0 to 10
 );
 
 CREATE TABLE Chapter(
@@ -58,8 +77,9 @@ CREATE TABLE ReviewChapter(
 	rating INTEGER,
 	chapterComment VARCHAR(512),
 	PRIMARY KEY (reviewID, chapterNumber),
-	FOREIGN KEY (reviewID) REFERENCES Review(reviewID) 
-	-- There needs to be a constraint that the chapter number must actually correspond to a chapter of the given game
+	FOREIGN KEY (reviewID) REFERENCES Review(reviewID),
+	CHECK (rating >= 0),
+	CHECK (rating <= 10)
 );
 
 
